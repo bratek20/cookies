d84de0cadb176541fd1f591a2ada051cb5acd0cb
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2023-09-21 07:01:50 +0200

    cleaning db after every test if you include db config in spring boot test classes

--- src/test/java/pl/bratek20/cookies/cookies/impl/ImplIntegrationTest.java
+++ src/test/java/pl/bratek20/cookies/cookies/impl/ImplIntegrationTest.java
@@ -1,18 +1,14 @@
 package pl.bratek20.cookies.cookies.impl;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
 import pl.bratek20.cookies.CookiesApplication;
 import pl.bratek20.cookies.cookies.api.ApiTest;
 import pl.bratek20.cookies.cookies.api.CookiesApi;
 import pl.bratek20.cookies.cookies.impl.infrastructure.configs.CookiesConfig;
-import pl.bratek20.cookies.test.dbcleaner.DBCleaner;
-import pl.bratek20.cookies.test.dbcleaner.DBCleanerConfig;
 import pl.bratek20.cookies.test.MySQLExtension;
-import pl.bratek20.cookies.test.dbcleaner.DBCleanerTestListener;
+import pl.bratek20.cookies.test.dbcleaner.DBCleanerConfig;
 
 @SpringBootTest(classes = {
     CookiesApplication.BaseConfig.class,
@@ -20,7 +16,6 @@ import pl.bratek20.cookies.test.dbcleaner.DBCleanerTestListener;
     DBCleanerConfig.class,
 })
 @ExtendWith(MySQLExtension.class)
-@TestExecutionListeners(listeners = DBCleanerTestListener.class)
 class ImplIntegrationTest extends ApiTest {
 
     @Autowired
--- src/test/java/pl/bratek20/cookies/test/dbcleaner/DBCleanerTestListener.java
+++ src/test/java/pl/bratek20/cookies/test/dbcleaner/DBCleanerTestListener.java
@@ -1,17 +1,17 @@
 package pl.bratek20.cookies.test.dbcleaner;
 
 import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListener;
 
 public class DBCleanerTestListener implements TestExecutionListener {
 
-    @Autowired
-    private DBCleaner dbCleaner;
-
     @Override
     public void afterTestMethod(@NotNull TestContext testContext) {
-        dbCleaner.deleteAllTables();
+        var context = testContext.getApplicationContext();
+        if (context.containsBean("dbCleaner")) {
+            var dbCleaner = context.getBean(DBCleaner.class);
+            dbCleaner.deleteAllTables();
+        }
     }
 }

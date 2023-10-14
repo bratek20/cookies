882d42dfdbd52b5276eb14e6c0c1d22aae4cc36c
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2023-10-14 04:36:32 +0200

    fixing integration tests

--- src/main/java/pl/bratek20/cookies/cookies/impl/infrastructure/repostiories/crud/CrudRepositoryConfig.java
+++ src/main/java/pl/bratek20/cookies/cookies/impl/infrastructure/repostiories/crud/CrudRepositoryConfig.java
@@ -1,15 +1,20 @@
 package pl.bratek20.cookies.cookies.impl.infrastructure.repostiories.crud;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import pl.bratek20.cookies.cookies.impl.application.CookiesRepository;
 
 @Configuration
 @Import({
-        DataSourceAutoConfiguration.class,
+    DataSourceAutoConfiguration.class,
+
 })
+@EnableJpaRepositories
+@EntityScan
 public class CrudRepositoryConfig {
 
     @Bean

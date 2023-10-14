07aad6607af3a060662b18e22a3f85a42d438bc9
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2023-10-14 15:16:39 +0200

    break code on purpose to fail integration tests

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cookies
 (
     id         INT AUTO_INCREMENT PRIMARY KEY,
-    identity_id BIGINT NOT NULL,
+    identity_id VARCHAR(64) NOT NULL,
     flavor     VARCHAR(64) NOT NULL,
     amount     INT NOT NULL
 );

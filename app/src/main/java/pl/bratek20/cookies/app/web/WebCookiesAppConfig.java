c950c6fbf1ba40b04bbf49206c1faecbeb360849
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2023-10-27 05:23:38 +0200

    test fixes

--- app/src/main/java/pl/bratek20/cookies/app/web/WebCookiesAppConfig.java
+++ app/src/main/java/pl/bratek20/cookies/app/web/WebCookiesAppConfig.java
@@ -3,6 +3,7 @@ package pl.bratek20.cookies.app.web;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import pl.bratek20.common.app.web.BaseWebConfig;
+import pl.bratek20.cookies.cookies.impl.infrastructure.configs.CookiesConfig;
 import pl.bratek20.cookies.cookies.impl.infrastructure.configs.CookiesInMemoryConfig;
 import pl.bratek20.cookies.cookies.web.CookiesWebServerConfig;
 import pl.bratek20.common.user.web.UserWebServerConfig;
@@ -10,7 +11,7 @@ import pl.bratek20.common.user.web.UserWebServerConfig;
 @Configuration
 @Import({
     BaseWebConfig.class,
-    CookiesInMemoryConfig.class,
+    CookiesConfig.class,
     CookiesWebServerConfig.class,
     UserWebServerConfig.class,
 })
